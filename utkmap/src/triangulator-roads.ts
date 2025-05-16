6cae201d8ce7babaab4ce0d2099d9d89c9a78883
Lucas Alexandre <lucasalexandre.s.cc@gmail.com>
2025-05-16 15:41:36 -0300

    feat: fix roads

--- utkmap/src/triangulator-roads.ts
+++ utkmap/src/triangulator-roads.ts
@@ -1,50 +1,48 @@
-import earcut from "earcut";
+import earcut from 'earcut';
 import { lineOffset } from '@turf/turf';
 
-import { FeatureCollection, Feature, LineString } from "geojson";
+import { FeatureCollection, Feature, LineString } from 'geojson';
 
-import { ILayerGeometry, ILayerComponent } from "./interfaces";
-import { Triangulator } from "./triangulator";
+import { ILayerGeometry, ILayerComponent } from './interfaces';
+import { Triangulator } from './triangulator';
 
 export class TriangulatorRoads extends Triangulator {
+  static override buildMesh(geojson: FeatureCollection, origin: number[]): [ILayerGeometry[], ILayerComponent[]] {
+    const mesh: ILayerGeometry[] = [];
+    const comps: ILayerComponent[] = [];
 
-    static override buildMesh(geojson: FeatureCollection, origin: number[]): [ILayerGeometry[], ILayerComponent[]] {
-        const mesh: ILayerGeometry[] = [];
-        const comps: ILayerComponent[] = [];
+    // translate based on origin
+    Triangulator.translateFeatures(geojson, origin);
 
-        // translate based on origin
-        Triangulator.translateFeatures(geojson, origin);
+    const collection: Feature[] = geojson['features'];
 
-        const collection: Feature[] = geojson['features'];
+    for (const feature of collection) {
+      //if(['cycleway', 'footway', 'pedestrian', 'proposed', 'construction', 'abandoned', 'platform', 'raceway'].includes(feature.properties?.highway)) {
+      //    continue;
+      //}
 
-        for (const feature of collection) {
-            if(['cycleway', 'footway', 'pedestrian', 'proposed', 'construction', 'abandoned', 'platform', 'raceway'].includes(feature.properties?.highway)) {
-                continue;
-            }
+      const base = <LineString>feature.geometry;
 
-            const base = <LineString>feature.geometry;
+      const top = lineOffset(base, 200, { units: 'kilometers' }).geometry.coordinates;
+      const bot = lineOffset(base, -200, { units: 'kilometers' }).geometry.coordinates;
 
-            const top = lineOffset(base, 200, {units: "kilometers"}).geometry.coordinates;
-            const bot = lineOffset(base,-200, {units: "kilometers"}).geometry.coordinates;
+      bot.forEach((cord: number[]) => top.unshift(cord));
+      top.push(top[0]);
 
-            bot.forEach((cord: number[]) => top.unshift(cord));
-            top.push(top[0]);
+      const flatIds = earcut(top.flat());
+      const flatCoords = top.map((cord: number[]) => [cord[0], cord[1], 0]).flat();
 
-            const flatIds = earcut(top.flat());
-            const flatCoords = top.map((cord: number[]) => [cord[0], cord[1], 0]).flat();
+      mesh.push({
+        position: flatCoords,
+        indices: flatIds,
+      });
 
-            mesh.push({
-                position: flatCoords,
-                indices: flatIds
-            });
+      comps.push({
+        nPoints: flatCoords.length / 3,
+        nTriangles: flatIds.length / 3,
+      });
+    }
 
-            comps.push({
-                nPoints: flatCoords.length / 3,
-                nTriangles: flatIds.length / 3
-            });
-        }
-
-        return [mesh, comps];
-    };
-
+    return [mesh, comps];
+  }
 }
