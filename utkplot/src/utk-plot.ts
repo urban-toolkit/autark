54f917022fd9dc4c7b1b8e1d8547b2e0c2894f65
Marcos Lage <mlage@ic.uff.br>
2025-06-13 15:02:41 -0300

    Map + Plot using vega - brush example.

--- utkplot/src/utk-plot.ts
+++ utkplot/src/utk-plot.ts
@@ -7,11 +7,9 @@ export abstract class UtkPlot {
     protected _view!: any; 
 
     protected _data!: GeoJsonProperties[];
-    protected _selectedIds!: number[] | string[];
-
     protected _plotEvents!: PlotEvents;
 
-    constructor(svg: HTMLElement, events: PlotEvent[] = [PlotEvent.CLICK]) {
+    constructor(svg: HTMLElement, events: PlotEvent[]) {
         this._div = svg;
         this._plotEvents = new PlotEvents(events);
     }
@@ -20,21 +18,20 @@ export abstract class UtkPlot {
         return this._view;
     }
 
-    get selectedIds(): number[] | string[] {
-        return this._selectedIds;
+    get data(): GeoJsonProperties[] {
+        return this._data;
     }
 
-    set selectedIds(selectedIds: number[] | string[]) {
-        this._selectedIds = selectedIds;
+    set data(data: GeoJsonProperties[]) {
+        this._data = data;
     }
 
     get plotEvents(): PlotEvents {
         return this._plotEvents;
     }
 
-    loadData(DataItem: GeoJsonProperties[]) {
-        this._data = DataItem;
-    }
-    
+
+    abstract configureSignalListeners(propName: string): void;
+
     abstract draw(): Promise<void>;
 }
