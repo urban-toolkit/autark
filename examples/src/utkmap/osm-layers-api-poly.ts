f78e594b9f68289fce1875407385749d41b30672
Marcos Lage <mlage@ic.uff.br>
2025-06-30 13:46:56 -0300

    Overpass query example using polygons.

---
+++ examples/src/utkmap/osm-layers-api-poly.ts
@@ -0,0 +1,73 @@
+import { UtkMap, LayerType } from 'utkmap';
+import { SpatialDb } from 'utkdb';
+
+export class OsmLayersApi {
+    protected map!: UtkMap;
+    protected db!: SpatialDb;
+
+    public async run(): Promise<void> {
+        this.db = new SpatialDb();
+        await this.db.init();
+
+        await this.db.loadOsmFromOverpassApi({
+            polygon: [ 
+                [-74.0065074140,40.6963494934],
+                [-74.0262219458,40.7000278019],
+                [-74.0186055774,40.7425129106],
+                [-73.9680946755,40.7355983529],
+                [-73.9738489526,40.7090665214],
+                [-73.9961058937,40.7060225978],
+                [-74.0065074140,40.6963494934]
+            ], 
+            outputTableName: 'table_osm',
+            autoLoadLayers: {
+                coordinateFormat: 'EPSG:3395',
+                layers: [
+                    'coastline',
+                    'parks',
+                    'water',
+                    'roads',
+                    // 'buildings',
+                ] as Array<'surface' | 'coastline' | 'parks' | 'water' | 'roads' | 'buildings'>,
+                dropOsmTable: true,
+            },
+        });
+
+
+        const canvas = document.querySelector('canvas');
+        if (canvas) {
+            canvas.width = canvas.height = canvas.parentElement?.clientHeight || 800;
+
+            this.map = new UtkMap(canvas);
+
+            await this.map.init(this.db.getOsmBoundingBox());
+            await this.loadLayers();
+
+            this.map.draw();
+        }
+    }
+
+    async loadLayers(): Promise<void> {
+        const data = [];
+        for (const layerData of this.db.tables) {
+
+            if (layerData.source === 'csv') {
+                continue;
+            }
+
+            const geojson = await this.db.getLayer(layerData.name);
+            data.push({ props: layerData, data: geojson });
+        }
+
+        for (const json of data) {
+            console.log(`Loading layer: ${json.props.name} of type ${json.props.type}`);
+            this.map.loadGeoJsonLayer(json.props.name, json.props.type as LayerType, json.data);
+        }
+    }
+}
+
+async function main() {
+    const example = new OsmLayersApi();
+    await example.run();
+}
+main();
