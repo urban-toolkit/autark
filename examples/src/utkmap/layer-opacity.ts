c557ff28fee2da571ce8861ed20b6d5bfd53136f
Marcos Lage <mlage@ic.uff.br>
2025-07-08 11:26:01 -0300

    Custom Lines Layer.

--- examples/src/utkmap/layer-opacity.ts
+++ examples/src/utkmap/layer-opacity.ts
@@ -30,7 +30,7 @@ export class LayerOpacity {
       geojsonFileUrl: 'http://localhost:5173/data/mnt_neighs.geojson',
       outputTableName: 'neighborhoods',
       coordinateFormat: 'EPSG:3395',
-      type: 'polygons',
+      type: 'features'
     });
 
     const canvas = document.querySelector('canvas');
---
+++ utkmap/src/triangulator-lines.ts
@@ -0,0 +1,44 @@
+import earcut from 'earcut';
+import { lineOffset } from '@turf/turf';
+
+import { FeatureCollection, Feature, LineString } from 'geojson';
+
+import { ILayerGeometry, ILayerComponent } from './interfaces';
+import { Triangulator } from './triangulator';
+
+export class TriangulatorLines extends Triangulator {
+    static offset: number = 300;
+
+    static override buildMesh(geojson: FeatureCollection, origin: number[]): [ILayerGeometry[], ILayerComponent[]] {
+        const mesh: ILayerGeometry[] = [];
+        const comps: ILayerComponent[] = [];
+
+        const collection: Feature[] = geojson['features'];
+
+        for (const feature of collection) {
+            const base = <LineString>feature.geometry;
+            base.coordinates = base.coordinates.map((cord: number[]) => [cord[0] - origin[0], cord[1] - origin[1]]);
+
+            const top = lineOffset(base,  TriangulatorLines.offset).geometry.coordinates;
+            const bot = lineOffset(base, -TriangulatorLines.offset).geometry.coordinates;
+
+            bot.forEach((cord: number[]) => top.unshift(cord));
+            top.push(top[0]);
+
+            const flatIds = earcut(top.flat());
+            const flatCoords = top.map((cord: number[]) => [cord[0], cord[1]]).flat();
+
+            mesh.push({
+                position: flatCoords,
+                indices: flatIds,
+            });
+
+            comps.push({
+                nPoints: flatCoords.length / 2,
+                nTriangles: flatIds.length / 3,
+            });
+        }
+
+        return [mesh, comps];
+    }
+}
