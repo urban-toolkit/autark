112d5e9db2152b26b9a7a0b21cd716126913f2a0
Lucas Alexandre <lucasalexandre.s.cc@gmail.com>
2025-09-28 15:07:24 -0300

    feat: create example to test autk-compute

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -1,39 +1,39 @@
 import { ViteDevServer, defineConfig } from 'vite';
 
 export function pluginWatchNodeModules(modules: string[]) {
-    const pattern = `/node_modules\\/(?!${modules.join('|')}).*/`;
-    return {
-        name: 'watch-node-modules',
-        configureServer: (server: ViteDevServer): void => {
-
-            server.watcher.options = {
-                ...server.watcher.options,
-                ignored: [new RegExp(pattern), '**/.git/**'],
-            };
-        },
-    };
+  const pattern = `/node_modules\\/(?!${modules.join('|')}).*/`;
+  return {
+    name: 'watch-node-modules',
+    configureServer: (server: ViteDevServer): void => {
+      server.watcher.options = {
+        ...server.watcher.options,
+        ignored: [new RegExp(pattern), '**/.git/**'],
+      };
+    },
+  };
 }
 
 export default defineConfig({
-    plugins: [pluginWatchNodeModules(['autk-map', 'autk-db', 'autk-plot'])],
-    optimizeDeps: {
-        exclude: ['autk-map', 'autk-db', 'autk-plot'],
+  plugins: [pluginWatchNodeModules(['autk-map', 'autk-db', 'autk-plot', 'autk-compute'])],
+  optimizeDeps: {
+    exclude: ['autk-map', 'autk-db', 'autk-plot', 'autk-compute'],
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        nested: '/src/autk-db/',
+        nested2: '/src/autk-map/',
+        nested3: '/src/autk-plot/',
+        nested4: '/src/autk-compute/',
+      },
     },
-    build: {
-        rollupOptions: {
-            input: {
-                nested : '/src/autk-db/',
-                nested2: '/src/autk-map/',
-                nested3: '/src/autk-plot/',
-            }
-        }
+  },
+  server: {
+    open: '/src/autk-compute/property-func.html',
+    cors: {
+      origin: '*',
+      allowedHeaders: 'Range, Content-Type, Authorization',
+      exposedHeaders: 'Content-Range',
     },
-    server: {
-        open: '/src/autk-plot/map-d3-brush.html',
-        cors: {
-            origin: '*',
-            allowedHeaders: 'Range, Content-Type, Authorization',
-            exposedHeaders: 'Content-Range'
-        },
-    },
+  },
 });
