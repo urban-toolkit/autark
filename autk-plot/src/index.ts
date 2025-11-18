5a547e7a67665bd170f7f0c340671fd661da6158
Marcos Lage <mlage@ic.uff.br>
2025-11-18 14:23:59 -0300

    Scatterplot with click.

--- autk-plot/src/index.ts
+++ autk-plot/src/index.ts
@@ -4,7 +4,6 @@ export * from './constants'
 export * from './plot-d3';
 export * from './plot-events';
 export * from './plot-style';
-// export * from './plot-vega';
 export * from './types';
 
 // plot types
